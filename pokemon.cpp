0d82233315977325ca9098735ca594065581b88c
Alex Macedo <macedo22@ucla.edu>
2016-11-20 23:34:29 -0800

    Overwrite pokemon files with new species and attacks

--- pokemon.cpp
+++ pokemon.cpp
@@ -1,59 +1,223 @@
+/*Alex Macedo
+Project 7
+Pokemon-clean
+email:macedo22@ucla.edu
+
+This includes the implementation for member functions of
+the Pokemon, Fire, Water, Grass, Squirtle, Wartortle, and
+Blastoise classes. Contains constructors for each pokemon
+type that modifies their stats according to type. Also
+includes the implementation of friend functions make_pokemon
+and and an ostream overloaded function. The former creates
+a pokemon of a certain type while the latter is used to
+print the specifics about a pokemon object.
+
+The virtual attack1 function is implemented in Water and
+Blastoise classes, while virtual attack2 function is
+implemented in the Squirtle, Wartortle, and Blastoise
+classes. Each attack functions calls the take_damage function
+to deplete an enemy's hp and print who attacked who and how
+much damage was incurred.*/
+
 #include "pokemon.h"
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
+//Overloads << operator to print information about a pokemon
+ostream& operator <<(ostream& os, Pokemon pokemon){
+	os << "Pokemon: " << endl
+		<< "\tID: " << pokemon.id << endl
+		<< "\tNick name: " << pokemon.nickName << endl
+		<< "\tAttack: " << pokemon.attack << endl
+		<< "\tDefense: " << pokemon.defense << endl
+		<< "\tEntry: " << endl
+		<< "\t\tNumber: " << (*(pokemon.pokeEntry)).number << endl
+		<< "\t\tName: " << (*(pokemon.pokeEntry)).name << endl << endl;
+
+	return os;
+}
+
+/*Uses the overloaded << operator to print a pokemon's information
+to a file named usin their unique id number*/
+void Pokemon::write(){
+	ofstream outFile;
+	string filename = id + ".dat";
+	outFile.open(filename);
+
+	outFile << (*this) << endl;
+
+	outFile.close();
+}
+
+
+/*Passes the damage given by a specific attack and it's elemental
+damage type. The weaknesses of the pokemon incurring damage
+will be iterated through. If a weakness equals one of the elemental
+attacks inflicted by the attacker, the attacked pokemon becomes
+weaker. The level of weakness increases the total damage incurred,
+which is subtracted from the attacked's current hp and is returned.*/
+int Pokemon::take_damage(int damageAmount, list<element> damageTypes){
+	int weaknessCount = 0;
+	for (auto wIt = weaknesses.begin(); wIt != weaknesses.end(); wIt++)
+		for (auto dIt = damageTypes.begin(); dIt != damageTypes.end(); dIt++)
+			if (*dIt == *wIt) weaknessCount++;
+
+	damageAmount = static_cast<int>(damageAmount * (log(5 * weaknessCount + 5) / log(5)));
+	currentHP -= damageAmount;
+	return damageAmount;
+}
+
+//stat modifications for Fire pokemon
 Fire::Fire(int index) : Pokemon(index){
-	maxHP+=1; speed+=2;
-	attack+=2; defense-=2;
-	specialAttack+=4; specialDefense+=1;
+	maxHP += 1; speed += 2;
+	attack += 2; defense -= 2;
+	specialAttack += 4; specialDefense += 1;
+	weaknesses.push_back(element::water);
 }
 
+//stat modifications for Water pokemon
 Water::Water(int index) : Pokemon(index){
-	maxHP+=2; speed-=1;
-	attack-=2; defense+=2;
-	specialAttack-=4; specialDefense-=1;
+	maxHP += 2; speed -= 1;
+	attack -= 2; defense += 2;
+	specialAttack -= 4; specialDefense -= 1;
+	weaknesses.push_back(element::grass);
 }
 
+//Bubble attack
+int Water::attack1(Pokemon* pokemon){
+	list<element> damageTypes;
+	damageTypes.push_back(element::fire);
+
+	int damage= (*pokemon).take_damage(2, damageTypes);
+
+	cout << (*this).get_nickName() << " used Bubble. "
+		<< (*pokemon).get_species() << " took "
+		<< damage << " damage!" << endl;
+	return damage;
+}
+
+//stat modifications for Grass pokemon
 Grass::Grass(int index) : Pokemon(index){
-	maxHP-=3; speed-=2;
-	attack-=2; defense+=0;
-	specialAttack-=3; specialDefense+=0;
+	maxHP -= 3; speed -= 2;
+	attack -= 2; defense += 0;
+	specialAttack -= 3; specialDefense += 0;
+	weaknesses.push_back(element::fire);
 }
 
-&ostream Pokemon::operator<<(ostream& os, const Pokemon& pokemon){
-    os<< "Pokemon: "<<endl
-        <<"\tID: " <<pokemon.id<<endl
-        <<"\tNick name: " <<pokemon.nickName<<endl
-        <<"\tAttack: " <<pokemon.attack<<endl
-        <<"\tDefense: " <<pokemon.defense<<endl
-        <<"\tEntry: "<<endl
-        <<"\t\tNumber: " <<pokemon.number<<endl
-        << "\t\tName: " <<pokemon.name<<endl<<endl;
+//Nick name defaults to Squirtle
+Squirtle::Squirtle(string n):Water(7){
+	set_nickName(n);
+	set_species("Squirtle");
 }
 
-Pokemon::write(){
-    std::ofstream outFile;
-    string filename = pokemon.id + ".dat";
-    outFile.open(filename);
+//Tackle attack
+int Squirtle::attack2(Pokemon* pokemon){
+	list<element> damageTypes;
+	damageTypes.push_back(element::normal);
 
-    outfile<<pokemon<<endl;
+	int damage = (*pokemon).take_damage(2, damageTypes);
 
-    outFile.close();
+	cout << (*this).get_nickName() << " used Tackle. "
+		<< (*pokemon).get_species() << " took "
+		<< damage << " damage!" << endl;
+	return damage;
 }
 
-Pokemon *make_pokemon(element e, std::string n) {
+//Nick name defaults to Wartortle
+Wartortle::Wartortle(string n):Water(8){
+	set_nickName(n);
+	set_species("Wartortle");
+}
+
+//Surf attack
+int Wartortle::attack2(Pokemon* pokemon){
+	list<element> damageTypes;
+	damageTypes.push_back(element::water);
+
+	int damage = (*pokemon).take_damage(4, damageTypes);
+
+	cout << (*this).get_nickName() << " used Surf. "
+		<< (*pokemon).get_species() << " took "
+		<< damage << " damage!" << endl;
+	return damage;
+}
+
+//Nick name defaults to Blastoise
+Blastoise::Blastoise(string n): Water(9){
+	set_nickName(n);
+	set_species("Blastoise");
+}
+
+//Water Pledge attack
+int Blastoise::attack1(Pokemon* pokemon){
+	list<element> damageTypes;
+	damageTypes.push_back(element::water);
+	damageTypes.push_back(element::dragon);
+
+	int damage = (*pokemon).take_damage(8, damageTypes);
+
+	cout << (*this).get_nickName() << " used Water Pledge. "
+		<< (*pokemon).get_species() << " took "
+		<< damage << " damage!" << endl;
+	return damage;
+}
+
+//Hydro Cannon attack
+int Blastoise::attack2(Pokemon* pokemon){
+	list<element> damageTypes;
+	damageTypes.push_back(element::water);
+
+	int damage = (*pokemon).take_damage(6, damageTypes);
+
+	cout << (*this).get_nickName() << " used Hydro Cannon. "
+		<< (*pokemon).get_species() << " took "
+		<< damage << " damage!" << endl;
+	return damage;
+}
+
+/*Creates and returns a pointer to a Squirtle, Wartortle, or
+Blastoise object by passing an index and nick name.*/
+
+Pokemon *make_pokemon(int index, string n) {
 
 	Pokemon *newPokemon;
-	if(e == element::fire) {
-		newPokemon = new Fire(1);
-		newPokemon->name = n;
-	} else if(e == element::grass) {
-		newPokemon = new Grass(1);
-		newPokemon->name = n;
-	} else if(e == element::water) {
-		newPokemon = new Water(1);
-		newPokemon->name = n;
+	if (index == 7) {
+		newPokemon = new Squirtle(n);
+		newPokemon->get_nickName() = n;
+		cout << "You created a new Squirtle named " << n << endl;
+	}
+	else if (index == 8) {
+		newPokemon = new Wartortle(n);
+		newPokemon->get_nickName() = n;
+		cout << "You created a new Wartortle named " << n << endl;
+	}
+	else if (index == 9) {
+		newPokemon = new Blastoise(n);
+		newPokemon->get_nickName() = n;
+		cout << "You created a new Blastoise named " << n << endl;
+	}
+	else{
+		cout << "Not a valid index. Choose 7, 8, or 9." << endl;
+		return NULL;
 	}
 	return newPokemon;
 
+}
+
+//simple main function to demonstrate functionality of attack functions
+int main(){
+
+	Squirtle squirtle = Squirtle("Splashy");
+	Pokemon* blastoise = make_pokemon(9, "Old Faithful");//test for make_pokemon function
+
+	squirtle.attack2(blastoise);//squirtle attacks blastoise
+	(*blastoise).attack1(&squirtle);//blastoise attacks squirtle
+
+	Pokemon* charmander = new Fire(1);
+	(*charmander).set_species("Charmander");
+	(*blastoise).attack1(charmander);//blastoise attacks charmander, who takes more damage than squirtle due to elemental weakness
+
+	return 0;
 }
--- pokemon.h
+++ pokemon.h
@@ -1,28 +1,53 @@
+/*Declaration of base class Pokemon, its derived
+classes (Fire, Water, and Grass) and the three species
+classes that derive from Water (Squirtle, Wartortle, and
+Blastoise. The pokemon class sets base stats that are
+modified in pokemon.cpp depending on elemental type.
+Derived classes have no unique functions other than their
+constructors, and define Pokemon's two virtual attack
+functions. Also declared are the make_pokemon function and
+overloaded << operator that are friends to the Pokemon
+class. The PokeEntry struct is used to store information
+unique to each pokemon from the pokedex.*/
+
 #ifndef POKEMON_H
 #define POKEMON_H
 
 #include <string>
+#include <fstream>
+#include <list>
+#include <cmath>
+
+using namespace std;
 
 enum class element {
-    fire = 0, water = 1, grass = 2
+	fire = 0, water = 1, grass = 2, normal = 3, dragon = 4
 };
 
 struct PokeEntry {
-    int number;
-    char name[20];
-    char type[10];
-    char description[100];
+	int number;
+	string name;//i.e. Jolteon
+	char type[10];
+	char description[100];
 
-    PokeEntry() {};
-    PokeEntry(int n){};
+	PokeEntry() {};
+	PokeEntry(int n) {
+		std::ifstream entryFile(std::to_string(n) + ".txt");
+		entryFile >> number;
+		entryFile >> name;
+		entryFile >> type;
+		entryFile.seekg(1, std::ios::cur);
+		entryFile.getline(description, 199);
+		entryFile.close();
+	};
 };
 
 
 class Pokemon {
 private:
-	std::string nickName;
+	std::string nickName;//i.e. "Sparky" for Jolteon
 	int level = 1;
-	string id;
+	string id;//unique hash identification number
 protected:
 	int maxHP = 20;
 	int currentHP = maxHP;
@@ -31,11 +56,18 @@ protected:
 	int defense = 10;
 	int specialAttack = 10;
 	int specialDefense = 10;
+	list<element> weaknesses;
+public:
+	PokeEntry* pokeEntry;
 
 public:
-	Pokemon(int i) {}
+	Pokemon(int i) {
+		pokeEntry = new PokeEntry(i);
+	}
 	~Pokemon() {}
-	std::string get_nickName() {
+
+	//getters
+	string get_nickName() {
 		return nickName;
 	}
 	int get_level() {
@@ -47,26 +79,61 @@ public:
 	int get_currentHP() {
 		return currentHP;
 	}
-	friend Pokemon* make_pokemon(element type, std::string name);
-	ostream& operator <<(&ostream os, const Pokemon& pokemon);
-	void write();
+	string get_species(){
+		return (*pokeEntry).name;
+	}
 
+	//stters
+	void set_nickName(string n){
+		nickName = n;
+	}
+	void set_species(string name){
+		(*pokeEntry).name = name;
+	}
+
+	void write();//uses overloaded << operator to print pokemon info to a file
+	int take_damage(int damageAmount, list<element> damageTypes);//inflicts damage on an opponent and prints details of the event
+
+	virtual int attack1(Pokemon* pokemon){ return 0; };//implemented by Water and Blastoise
+	virtual int attack2(Pokemon* pokemon){ return 0; };//implemented by Squirtle, Wartortle, and Blastoise
+
+	friend Pokemon* make_pokemon(int index, string n);//creates new pokemon based on pokedex index and nick name
+	friend ostream& operator <<(ostream& os, Pokemon pokemon);//used to print information about a pokemon
 };
 
-class Fire: public Pokemon {
+class Fire : public Pokemon {
 public:
 	Fire(int index);
 };
 
-class Water: public Pokemon {
+class Water : public Pokemon {
 public:
 	Water(int index);
+	int attack1(Pokemon* pokemon);
 };
 
-class Grass: public Pokemon {
+class Grass : public Pokemon {
 public:
 	Grass(int index);
 };
 
-#endif // POKEMON_H
+class Squirtle : public Water{
+public:
+	Squirtle(string n="Squirtle");
+	int attack2(Pokemon* pokemon);
+};
 
+class Wartortle : public Water {
+public:
+	Wartortle(string n="Wartortle");
+	int attack2(Pokemon* pokemon);
+};
+
+class Blastoise : public Water {
+public:
+	Blastoise(string n = "Blastoise");
+	int attack1(Pokemon* pokemon);
+	int attack2(Pokemon* pokemon);
+};
+
+#endif // POKEMON_H
